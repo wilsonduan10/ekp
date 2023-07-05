5ef65c17b4ca63a66ae643543c844dc31b945eb6
Wilson Duan <wduan@caltech.edu>
2023-07-05 16:53:25 -0700

    todays changes

--- businger_calibration.jl
+++ businger_calibration.jl
@@ -5,7 +5,7 @@ using EnsembleKalmanProcesses.ParameterDistributions
 const EKP = EnsembleKalmanProcesses
 
 using Downloads
-using DelimitedFiles
+using NCDatasets
 
 using CLIMAParameters
 const CP = CLIMAParameters
@@ -21,25 +21,19 @@ include("setup_parameter_set.jl")
 
 mkpath(joinpath(@__DIR__, "data")) # create data folder if not exists
 localfile = "data/Stats.cfsite17_CNRM-CM5_amip_2004-2008.10.nc"
+data = NCDataset(localfile);
 
-
-
-
-data_mean_velocity = readdlm("data/profiles.dat", skipstart = 112) ## We skip 72 lines (header) and 40(laminar layer)
-
-
-
-
-
-
-
-
-
-
-# We extract the required info for this problem
-u_star_obs = 4.14872e-02
-z = data_mean_velocity[:, 1]
-u = data_mean_velocity[:, 3] * u_star_obs
+# Construct observables
+# try different observables - ustar, L_MO, flux (momentum, heat, buoyancy)
+# instead of using u, use √u^2 + v^2
+# first try ustar
+time_data = Array(data.group["timeseries"]["t"]) # (865, )
+z_data = Array(data.group["profiles"]["z"]) # (200, )
+u_star_data = Array(data.group["timeseries"]["friction_velocity_mean"]) # (865, ) likely meaned over all z
+u_data = Array(data.group["profiles"]["u_mean"]) # (200, 865)
+# for i in 1:size(u_data)[1]
+#     u_data[:, i] = u_data[:, i] * u_star_data[i]
+# end
 
 function get_surf_flux_params(overrides)
     # First, we set up thermodynamic parameters
@@ -49,7 +43,7 @@ function get_surf_flux_params(overrides)
     thermo_params = SFP.thermodynamics_params(param_set)
 
     ## in this idealized case, we assume dry isothermal conditions
-    ts_sfc = TD.PhaseEquil_ρθq(thermo_params, FT(1), FT(300), FT(0))
+    ts_sfc = TD.PhaseEquil_ρθq(thermo_params, FT(1), FT(300), FT(0)) # 1, 300, 0.0
     ts_in = TD.PhaseEquil_ρθq(thermo_params, FT(1), FT(300), FT(0))
 
     # initialize κ parameter
@@ -85,34 +79,37 @@ end
 
 function physical_model(parameters, inputs)
     a_m, a_h = parameters
-    (; u, z) = inputs
+    (; u, z, time) = inputs
 
     overrides = (; a_m, a_h)
     surf_flux_params, ts_sfc, ts_in = get_surf_flux_params(overrides)
 
     # Now, we loop over all the observations and call SF.surface_conditions to estimate u^*
-    u_star = zeros(length(u))
-    for i in 1:lastindex(u)
-        u_in = u[i]
-        v_in = FT(0)
-        z_in = z[i]
-        u_in = SVector{2, FT}(u_in, v_in)
-        u_sfc = SVector{2, FT}(FT(0), FT(0))
+    u_star = zeros(length(time)) # (865, )
+    for j in 1:lastindex(time)
+        u_star_sum = 0.0
+        for i in 1:size(u_data)[1]
+            u_in = u[i, j]
+            v_in = FT(0)
+            z_in = z[i]
+            u_in = SVector{2, FT}(u_in, v_in)
+            u_sfc = SVector{2, FT}(FT(0), FT(0))
 
-        state_sfc = SF.SurfaceValues(FT(0), u_sfc, ts_sfc)
-        state_in = SF.InteriorValues(z_in, u_in, ts_in)
+            state_sfc = SF.SurfaceValues(FT(0), u_sfc, ts_sfc)
+            state_in = SF.InteriorValues(z_in, u_in, ts_in)
 
-        # We provide a few additional parameters for SF.surface_conditions
-        z0m = z0b = FT(0.0001)
-        gustiness = FT(0)
-        kwargs = (; state_in, state_sfc, z0m, z0b, gustiness)
-        sc = SF.ValuesOnly{FT}(; kwargs...)
+            # We provide a few additional parameters for SF.surface_conditions
+            z0m = z0b = FT(0.0001)
+            gustiness = FT(0)
+            kwargs = (; state_in, state_sfc, z0m, z0b, gustiness)
+            sc = SF.ValuesOnly{FT}(; kwargs...)
 
-        # Now, we call surface_conditions and store the calculated ustar:
-        sf = SF.surface_conditions(surf_flux_params, sc)
-        u_star[i] = sf.ustar  # TODO: also try for u_profiles[i, :] = sf.u_profile(z)
+            # Now, we call surface_conditions and store the calculated ustar:
+            sf = SF.surface_conditions(surf_flux_params, sc)
+            u_star_sum += sf.ustar
+        end
+        u_star[j] = u_star_sum / length(u)
     end
-
     return u_star
 end
 
@@ -120,21 +117,15 @@ end
 # from the dataset. The observable we elect is the mean of the calculated ustar across
 # all z, which is eventually compared to the actual observed ustar.
 function G(parameters, inputs)
-    u_star = physical_model(parameters, inputs)
-    u_star_mean = mean(u_star) # H map
-    return [u_star_mean]
+    u_star = physical_model(parameters, inputs) # (865, )
+    return u_star
 end
 
-# Define the inputs to be passed into G:
-u = u[1:(end - 1)] # we remove the last line because we want different surface state conditions
-z = z[1:(end - 1)]
-inputs = (; u, z)
+inputs = (u = u_data, z = z_data, time = time_data)
 
 Γ = 0.0005 * I
-η_dist = MvNormal(zeros(1), Γ)
-y = [u_star_obs] .+ rand(η_dist) # (H ⊙ Ψ ⊙ T^{-1})(θ) + η from Cleary et al 2021
-## we can try these definitions of y later: y = G(inputs, parameters) .+ rand(η_dist)
-## y = u - u_star/κ (log(z/z0))
+η_dist = MvNormal(zeros(length(u_star_data)), Γ)
+y = u_star_data .+ rand(η_dist) # (H ⊙ Ψ ⊙ T^{-1})(θ) + η from Cleary et al 2021
 
 # Assume that users have prior knowledge of approximate truth.
 # (e.g. via physical models / subset of obs / physical laws.)
@@ -144,7 +135,7 @@ prior = combine_distributions([prior_u1, prior_u2])
 
 # Set up the initial ensembles
 N_ensemble = 5;
-N_iterations = 10;
+N_iterations = 5;
 
 rng_seed = 41
 rng = Random.MersenneTwister(rng_seed)
@@ -170,12 +161,13 @@ final_ensemble = get_ϕ_final(prior, ensemble_kalman_process)
 # - The output of the physical model given the true κ = 0.4
 
 # We then compare them to the initial ensemble and the final ensemble.
-zrange = z
+zrange = z_data
+timestep = 1
 ENV["GKSwstype"] = "nul"
-theta_true = (0.0, 0.0)
+theta_true = (4.7, 4.7)
 plot(
     zrange,
-    physical_model(theta_true, inputs),
+    physical_model(theta_true, inputs)[timestep],
     c = :black,
     label = "Model Truth",
     legend = :bottomright,
@@ -184,35 +176,26 @@ plot(
 )
 plot!(
     zrange,
-    physical_model((100, 100), inputs),
-    c = :red,
-    label = "Test",
+    ones(length(zrange)) .* y[timestep],
+    c = :black,
+    label = "y",
     legend = :bottomright,
     linewidth = 2,
     linestyle = :dot,
 )
-# plot!(
-#     zrange,
-#     ones(length(zrange)) .* y,
-#     c = :black,
-#     label = "y",
-#     legend = :bottomright,
-#     linewidth = 2,
-#     linestyle = :dot,
-# )
-# plot!(zrange, ones(length(zrange)) .* u_star_obs, c = :black, label = "Truth u*", legend = :bottomright, linewidth = 2)
-# plot!(
-#     zrange,
-#     [physical_model(initial_ensemble[:, i], inputs) for i in 1:N_ensemble],
-#     c = :red,
-#     label = reshape(vcat(["Initial ensemble"], ["" for i in 1:(N_ensemble - 1)]), 1, N_ensemble), # reshape to convert from vector to matrix
-# )
-# plot!(
-#     zrange,
-#     [physical_model(final_ensemble[:, i], inputs) for i in 1:N_ensemble],
-#     c = :blue,
-#     label = reshape(vcat(["Final ensemble"], ["" for i in 1:(N_ensemble - 1)]), 1, N_ensemble),
-# )
+plot!(zrange, ones(length(zrange)) .* u_star_data[timestep], c = :black, label = "Truth u*", legend = :bottomright, linewidth = 2)
+plot!(
+    zrange,
+    [physical_model(initial_ensemble[:, i], inputs)[timestep] for i in 1:N_ensemble],
+    c = :red,
+    label = reshape(vcat(["Initial ensemble"], ["" for i in 1:(N_ensemble - 1)]), 1, N_ensemble), # reshape to convert from vector to matrix
+)
+plot!(
+    zrange,
+    [physical_model(final_ensemble[:, i], inputs)[timestep] for i in 1:N_ensemble],
+    c = :blue,
+    label = reshape(vcat(["Final ensemble"], ["" for i in 1:(N_ensemble - 1)]), 1, N_ensemble),
+)
 xlabel!("Z")
 ylabel!("U^*")
 png("our_plot")
