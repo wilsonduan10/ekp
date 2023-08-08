318f6a41f667937117c7160aa1d2135e41cd2e21
Wilson Duan <wduan@caltech.edu>
2023-08-08 10:22:47 -0700

    more changes

--- alt.jl
+++ alt.jl
@@ -2,9 +2,9 @@
 In this file, we use the ensemble kalman inversion process to calibrate four parameters:
 a\_m, a\_h, b\_m, and b\_h in the Businger stability functions, where a_m and a_h are parameters
 in the stable regime, and b_m and b_h pertain to the unstable regime. We use data from Shen 2022,
-cfSite LES data with GCM forcings. We use u_star as our observable, and predict u_star using 
+cfSite LES data with GCM forcings. We use u_star as our observable, and predict u_star using
 the function surface_conditions from the SurfaceFluxes.jl package, which uses the Monin-Obukhov
-similarity theory to calculate important scales such as the monin-obukhov length and u_star. The 
+similarity theory to calculate important scales such as the monin-obukhov length and u_star. The
 goal of this process is to analyze the efficacy of EKP in the context of surface fluxes. We visualize
 the success of the model through several plots.
 =#
@@ -42,33 +42,22 @@ include("helper/graph.jl")
 # We must first download the netCDF datasets and place them into the data/ directory. We have the option to choose
 # the cfsite and the month where data is taken from, as long as the data has been downloaded.
 mkpath(joinpath(@__DIR__, "data")) # create data folder if not exists
-cfsite = 10
+cfsite = 23
 month = "07"
 localfile = "data/Stats.cfsite$(cfsite)_CNRM-CM5_amip_2004-2008.$(month).nc"
 data = NCDataset(localfile)
 
 # We extract the relevant data points for our pipeline.
-max_z_index = 5
-spin_up = 100
-
-# profiles
-u_data = Array(data.group["profiles"]["u_mean"])[1:max_z_index, spin_up:end] # (200, 865)
-v_data = Array(data.group["profiles"]["v_mean"])[1:max_z_index, spin_up:end] # (200, 865)
-qt_data = Array(data.group["profiles"]["qt_mean"])[1:max_z_index, spin_up:end] # (200, 865)
-θ_li_data = Array(data.group["profiles"]["thetali_mean"])[1:max_z_index, spin_up:end] # (200, 865)
-temp_data = Array(data.group["profiles"]["temperature_mean"]) # (200, 865)
-
-# reference
-z_data = Array(data.group["profiles"]["z"])[1:max_z_index] # (200, )
-ρ_data = Array(data.group["reference"]["rho0"])[1:max_z_index] # (200, )
-p_data = Array(data.group["reference"]["p0"]) # (200, )
-
-# timeseries
-time_data = Array(data.group["timeseries"]["t"])[spin_up:end] # (865, )
-u_star_data = Array(data.group["timeseries"]["friction_velocity_mean"])[spin_up:end] # (865, )
-lhf_data = Array(data.group["timeseries"]["lhf_surface_mean"])[spin_up:end] # (865, )
-shf_data = Array(data.group["timeseries"]["shf_surface_mean"])[spin_up:end] # (865, )
-surface_temp_data = Array(data.group["timeseries"]["surface_temperature"])[spin_up:end]
+time_data = Array(data.group["timeseries"]["t"]) # (865, )
+z_data = Array(data.group["profiles"]["z"]) # (200, )
+u_star_data = Array(data.group["timeseries"]["friction_velocity_mean"]) # (865, )
+u_data = Array(data.group["profiles"]["u_mean"]) # (200, 865)
+v_data = Array(data.group["profiles"]["v_mean"]) # (200, 865)
+ρ_data = Array(data.group["reference"]["rho0"]) # (200, )
+qt_data = Array(data.group["profiles"]["qt_min"]) # (200, 865)
+θ_li_data = Array(data.group["profiles"]["thetali_mean"]) # (200, 865)
+lhf_data = Array(data.group["timeseries"]["lhf_surface_mean"]) # (865, )
+shf_data = Array(data.group["timeseries"]["shf_surface_mean"]) # (865, )
 
 Z, T = size(u_data) # extract dimensions for easier indexing
 
@@ -84,7 +73,7 @@ unconverged_z = Dict{FT, Int64}()
 unconverged_t = Dict{FT, Int64}()
 
 # We define our physical model. It takes in the parameters a\_m, a\_h, b\_m, b\_h, as well as data
-# inputs. It establishes thermodynamic parameters and Businger parameters in order to call the 
+# inputs. It establishes thermodynamic parameters and Businger parameters in order to call the
 # function surface_conditions. We store each time step's u_star and return a list of these u_stars.
 function physical_model(parameters, inputs)
     a_m, a_h, b_m, b_h = parameters
@@ -98,47 +87,46 @@ function physical_model(parameters, inputs)
     for j in 1:T # 865
         u_star_sum = 0.0
         total = 0
-        # Define surface conditions based on moist air density, liquid ice potential temperature, and total specific humidity 
-        # given from cfSite. 
+        # Define surface conditions based on moist air density, liquid ice potential temperature, and total specific humidity
+        # given from cfSite.
         ts_sfc = TD.PhaseEquil_ρθq(thermo_params, ρ_data[1], θ_li_data[1, j], qt_data[1, j]) # use 1 to get surface conditions
-        # ts_sfc = TD.PhaseEquil_pTq(thermo_params, p_data[1], surface_temp_data[j], qt_data[1, j])
         u_sfc = SVector{2, FT}(FT(0), FT(0))
         state_sfc = SF.SurfaceValues(FT(0), u_sfc, ts_sfc)
 
         # We now loop through all heights at this time step.
-        for i in 1:Z # starting at 2 because index 1 is our surface conditions
+        for i in 2:Z # starting at 2 because index 1 is our surface conditions
             u_in = u[i, j]
             v_in = FT(0)
             z_in = z[i]
             u_in = SVector{2, FT}(u_in, v_in)
-            
+
             ts_in = TD.PhaseEquil_ρθq(thermo_params, ρ_data[i], θ_li_data[i, j], qt_data[i, j])
-            # ts_in = TD.PhaseEquil_ρTq(thermo_params, p_data[i], temp_data[i, j], qt_data[i, j])
             state_in = SF.InteriorValues(z_in, u_in, ts_in)
 
             # We provide a few additional parameters for SF.surface_conditions
             z0m = z0b = z0
             gustiness = FT(1)
-            # kwargs = (state_in = state_in, state_sfc = state_sfc, shf = shf[j], lhf = lhf[j], z0m = z0m, z0b = z0b, gustiness = gustiness)
-            # sc = SF.Fluxes{FT}(; kwargs...)
-            kwargs = (state_in = state_in, state_sfc = state_sfc, z0m = z0m, z0b = z0b, gustiness = gustiness)
-            sc = SF.ValuesOnly{FT}(; kwargs...)
+            kwargs = (state_in = state_in, state_sfc = state_sfc, shf = shf[j], lhf = lhf[j], z0m = z0m, z0b = z0b, gustiness = gustiness)
+            sc = SF.Fluxes{FT}(; kwargs...)
+            # kwargs = (state_in = state_in, state_sfc = state_sfc, z0m = z0m, z0b = z0b, gustiness = gustiness)
+            # sc = SF.ValuesOnly{FT}(; kwargs...)
 
             # Now, we call surface_conditions and store the calculated ustar. We surround it in a try catch
             # to account for unconverged fluxes.
             try
-                sf = SF.surface_conditions(surf_flux_params, sc, soltype = RS.VerboseSolution())
+                sf = SF.surface_conditions(surf_flux_params, sc, soltype=RS.VerboseSolution())
                 u_star_sum += sf.ustar
                 total += 1
             catch e
                 println(e)
+
                 z_temp, t_temp = (z_data[i], time_data[j])
                 temp_key = (z_temp, t_temp)
                 haskey(unconverged_data, temp_key) ? unconverged_data[temp_key] += 1 : unconverged_data[temp_key] = 1
                 haskey(unconverged_z, z_temp) ? unconverged_z[z_temp] += 1 : unconverged_z[z_temp] = 1
                 haskey(unconverged_t, t_temp) ? unconverged_t[t_temp] += 1 : unconverged_t[t_temp] = 1
             end
-            
+
         end
 
         # We average the ustar over all heights and store it.
@@ -166,8 +154,16 @@ for u_star in u_star_data
 end
 variance /= T
 
-Γ = variance * I
-y = u_star_data
+# old
+# Γ = variance * I
+# y = u_star_data
+
+# new
+theta_true = [4.7, 4.7, 15.0, 9.0]
+dim_output = T
+Γ = 0.01^2 * I
+noise_dist = MvNormal(zeros(dim_output), Γ)
+y = G(theta_true, inputs) .+ rand(noise_dist)
 
 # Define the prior parameter values which we wish to recover in our pipeline. They are constrained
 # to be non-negative due to physical laws, and their mean is given by Businger et al 1971.
@@ -179,7 +175,7 @@ prior = combine_distributions([prior_u1, prior_u2, prior_u3, prior_u4])
 
 # Hyperparameters: we find it sufficient to define just 5 ensembles and iterations.
 N_ensemble = 5
-N_iterations = 5
+N_iterations = 10
 
 # Define EKP process.
 rng_seed = 41
@@ -187,6 +183,7 @@ rng = Random.MersenneTwister(rng_seed)
 initial_ensemble = EKP.construct_initial_ensemble(rng, prior, N_ensemble)
 
 ensemble_kalman_process = EKP.EnsembleKalmanProcess(initial_ensemble, y, Γ, Inversion(); rng = rng)
+
 # We run ensemble kalman inversrion for N_iterations
 for n in 1:N_iterations
     params_i = get_ϕ_final(prior, ensemble_kalman_process)
@@ -206,7 +203,9 @@ plot_params = (;
     prior = prior,
     model = physical_model,
     inputs = inputs,
+    # theta_true = (0.0, 0.0, 0.0, 0.0),
     theta_true = (4.7, 4.7, 15.0, 9.0),
+    theta_bad = (100.0, 100.0, 100.0, 100.0),
     ensembles = (constrained_initial_ensemble, final_ensemble),
     N_ensemble = N_ensemble,
     most_inputs = (u = u_data, z = z_data, time = time_data, lhf = lhf_data, shf = shf_data),
@@ -224,7 +223,7 @@ if (length(unconverged_data) > 0)
 end
 
 # We print the mean parameters of the initial and final ensemble to identify how
-# the parameters evolved to fit the dataset. 
+# the parameters evolved to fit the dataset.
 println("INITIAL ENSEMBLE STATISTICS")
 println("Mean a_m:", mean(constrained_initial_ensemble[1, :])) # [param, ens_no]
 println("Mean a_h:", mean(constrained_initial_ensemble[2, :]))
